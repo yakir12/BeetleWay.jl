ff88ff6d8905df9f8831dfd629651b70de69105d
yakir12 <12.yakir@gmail.com>
2017-09-07 16:25:02 +0200

    start working on the coverage of associations

--- src/BeetleWay.jl
+++ src/BeetleWay.jl
@@ -1,6 +1,7 @@
+__precompile__() 
 module BeetleWay
 
-using Gtk.ShortNames, GtkReactive, DataStructures, HDF5
+using Gtk.ShortNames, GtkReactive, DataStructures#, HDF5
 
 # patches
 # include(joinpath(@__DIR__, "patches.jl"))
@@ -9,11 +10,11 @@ include(joinpath(@__DIR__, "log", "gui.jl"))
 include(joinpath(@__DIR__, "log", "preliminary_report.jl"))
 include(joinpath(@__DIR__, "track", "segment.jl"))
 
+b = Builder(filename=joinpath(@__DIR__, "head.glade"))
 
 # folder = open_dialog("Select Video Folder", action=Gtk.GtkFileChooserAction.SELECT_FOLDER)
 folder = joinpath(first(splitdir(@__DIR__)), "test", "videofolder")
 
-b = Builder(filename=joinpath(@__DIR__, "head.glade"))
 id1 = signal_connect(_ -> log_gui(folder), b["start.log"], :clicked)
 id2 = signal_connect(_ -> report_gui(folder), b["preliminary.report"], :activate)
 id3 = signal_connect(_ -> fragment(folder), b["segment.videos"], :activate)
@@ -29,4 +30,3 @@ end # module
 # check the integratiy of the metadata
 # maybe clearer error messages
 # fix the travis errors!!!
-
