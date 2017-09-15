c920dbfad4d19b71ffcb367de5a622a9d3dc69cb
yakir12 <12.yakir@gmail.com>
2017-09-15 21:33:36 +0200

    last

--- src/BeetleWay.jl
+++ src/BeetleWay.jl
@@ -1,94 +1,80 @@
 # __precompile__() 
 module BeetleWay
 
-using Gtk.ShortNames, GtkReactive, JLD
+# const folder = Signal(open_dialog("Select Video Folder", action=Gtk.GtkFileChooserAction.SELECT_FOLDER))
+const folder = joinpath(src_dir, "..", "test", "videofolder")
 
+using Gtk.ShortNames, GtkReactive, JLD, VideoIO
+
+# directories
 # const src_dir = @__DIR__
 const src_dir = joinpath(Pkg.dir("BeetleWay"), "src")
+const prelog_dir = joinpath(src_dir, "prelog")
 const log_dir = joinpath(src_dir, "log")
+const postlog_dir = joinpath(src_dir, "postlog")
 const track_dir = joinpath(src_dir, "track")
 
-include(joinpath(log_dir, "associations.jl"))
-include(joinpath(log_dir, "glade_maker.jl"))
+# visibility constants
+const head_vis = Signal(true)
+const log_vis = Signal(false)
+const poi_vis = Signal(false)
+const run_vis = Signal(false)
+const checktime_vis = Signal(false)
 
-# folder = Signal(open_dialog("Select Video Folder", action=Gtk.GtkFileChooserAction.SELECT_FOLDER))
-folder = joinpath(src_dir, "..", "test", "videofolder")
+
+include(joinpath(prelog_dir, "associations.jl"))
 
 association = Association(folder)
-metadata = association.md
+const metadata = association.md
 
-# all the windows:
-head_builder = Builder(filename=joinpath(src_dir, "head.glade"))
-head_vis = Signal(true)
-# showall(head_builder["window"])
+# load all the GUI builders and set their visibility
+const head_builder = Builder(filename=joinpath(src_dir, "head.glade"))
 foreach(x -> visible(head_builder["window"], x), head_vis)
 
-log_builder = Builder(filename=joinpath(log_dir, "log.glade"))
-log_vis = Signal(false)
-# showall(log_builder["window"])
+const log_builder = Builder(filename=joinpath(log_dir, "log.glade"))
 foreach(x -> visible(log_builder["window"], x), log_vis)
 
-
-poi_builder = Builder(filename=joinpath(log_dir, "poi.glade"))
-poi_vis = Signal(false)
-# showall(poi_builder["window"])
+const poi_builder = Builder(filename=joinpath(log_dir, "poi.glade"))
 foreach(x -> visible(poi_builder["window"], x), poi_vis)
 
-glade_widgets = [typeof(l) => replace(f, ' ', '_') for (l,f) in zip(metadata.levels, metadata.factors)]
-parse2glade(glade_widgets)
-
-run_builder = Builder(filename=joinpath(log_dir, "run.glade"))
-run_vis = Signal(false)
-# showall(run_builder["window.run.wJqRk"])
+include(joinpath(prelog_dir, "glade_maker.jl"))
+const run_builder = Builder(filename=joinpath(log_dir, "run.glade"))
 foreach(x -> visible(run_builder["window.run.wJqRk"], x), run_vis)
 
-video_builder = Builder(filename=joinpath(log_dir, "video.glade"))
-video_vis = Signal(false)
-# showall(video_builder["window"])
-foreach(x -> visible(video_builder["window"], x), video_vis)
+const checktime_builder = Builder(filename=joinpath(postlog_dir, "checktime.glade"))
+foreach(x -> visible(checktime_builder["window"], x), checktime_vis)
 
 # track_builder = Builder(filename=joinpath(track_dir, "track.glade"))
 # track_vis = Signal(false)
 # showall(track_builder["window"])
 # foreach(x -> visible(track_builder["window"], x), track_vis)
 
+include(joinpath(log_dir, "main.jl"))
+include(joinpath(postlog_dir, "main.jl"))
 
-include(joinpath(log_dir, "gui.jl"))
+# head window widgets
 
-# signals
-
-# widgets
 start_log = button(widget=head_builder["start.log"])
-
-# id1 = signal_connect(_ -> log_gui(a), head_builder["start.log"], :clicked)
-# id2 = signal_connect(_ -> report_gui(a), head_builder["preliminary.report"], :activate)
-# id3 = signal_connect(_ -> fragment(a), head_builder["segment.videos"], :activate)
-# id4 = signal_connect(_ -> coordinates_gui(a), head_builder["track"], :clicked)
-
-video_id = signal_connect(log_builder["check.videos"], :activate) do _
-    push!(head_vis, false)
-    push!(video_vis, true)
-end
-
-split_videos_id = signal_connect(log_builder["split.videos"], :activate) do _
-end
-
-preliminary_report_id = signal_connect(log_builder["preliminary.report"], :activate) do _
-end
-
-# functions
 foreach(start_log, init=nothing) do _
     push!(head_vis, false)
     push!(log_vis, true)
     nothing
 end
 
+checktime_id = signal_connect(head_builder["check.videos"], :activate) do _
+    push!(head_vis, false)
+    push!(checktime_vis, true)
+end
 
+split_videos_id = signal_connect(head_builder["split.videos"], :activate) do _
+end
+
+preliminary_report_id = signal_connect(head_builder["preliminary.report"], :activate) do _
+end
+
+quit_id = signal_connect(head_builder["quit"], :activate) do _
+    quit()
+end
 
 
 end # module
-
-# TODO:
-# check the integratiy of the metadata
-# maybe clearer error messages
-# fix the travis errors!!!
--- src/patch.Gtk.open_dialog.jl
+++
@@ -1,6 +0,0 @@
-file = joinpath(Pkg.dir("Gtk"), "src", "selectors.jl")
-open(file, "r") do o
-    map(_ -> readline(o), 1:81)
-    l = readline(o)
-    r"yield"(l) || run(`sed -i '82i yield()' $file`)
-end
